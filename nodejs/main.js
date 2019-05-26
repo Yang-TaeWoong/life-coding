f17f77d0bc39356db2ab47f469956c02c7f1a669
Tae_woong <ytw5754@naver.com>
2019-05-27 00:22:47 +0900

    5/27
    can change the content and title of the file

--- nodejs/main.js
+++ nodejs/main.js
@@ -1,22 +1,24 @@
 var http = require('http');
 var fs = require('fs');
 var url = require('url');
+var qs = require('querystring');
 
-function templateHtml(title, list, body) {
-    return `<!doctype html>
-          <html>
-          <head>
-            <title>WEB1 - ${title}</title>
-            <meta charset="utf-8">
-          </head>
-          <body>
-            <h1><a href="/">WEB</a></h1>
-            ${list}
-            <a href="/create">create</a>
-            ${body}
-          </body>
-          </html>
-          `;
+function templateHTML(title, list, body, control) {
+    return `
+  <!doctype html>
+  <html>
+  <head>
+    <title>WEB1 - ${title}</title>
+    <meta charset="utf-8">
+  </head>
+  <body>
+    <h1><a href="/">WEB</a></h1>
+    ${list}
+    ${control}
+    ${body}
+  </body>
+  </html>
+  `;
 }
 
 function templateList(filelist) {
@@ -36,50 +38,100 @@ var app = http.createServer(function (request, response) {
     var pathname = url.parse(_url, true).pathname;
     if (pathname === '/') {
         if (queryData.id === undefined) {
-
             fs.readdir('./data', function (error, filelist) {
                 var title = 'Welcome';
                 var description = 'Hello, Node.js';
-                var list = '<ul>';
-                var i = 0;
                 var list = templateList(filelist);
-                var template = templateHtml(title, list, `<h2>${title}</h2>${description}`);
+                var template = templateHTML(title, list, `<h2>${title}</h2>${description}`,
+                    `<a href="/create">create</a>`);
                 response.writeHead(200);
                 response.end(template);
-            })
-
-
-
+            });
         } else {
             fs.readdir('./data', function (error, filelist) {
                 fs.readFile(`data/${queryData.id}`, 'utf8', function (err, description) {
                     var title = queryData.id;
                     var list = templateList(filelist);
-                    var template = templateHtml(title, list, `<h2>${title}</h2>${description}`);
+                    var template = templateHTML(title, list, `<h2>${title}</h2>${description}`, `<a href="/create">create</a> <a href="/update?id=${title}">update</a>`);
                     response.writeHead(200);
                     response.end(template);
                 });
             });
         }
-    } else if (pathname == '/create') {
+    } else if (pathname === '/create') {
         fs.readdir('./data', function (error, filelist) {
-            var title = 'web ';
-            var description = 'Hello, Node.js';
-            var list = '<ul>';
-            var i = 0;
+            var title = 'WEB - create';
             var list = templateList(filelist);
-            var template = templateHtml(title, list, `<form action="http://localhost:3000/process_create"  method="post">
-                        <p><input type="text" name="title" placeholder="title"></p>
-
-                        <p>
-                        <textarea name="decription" placeholder="description"> </textarea> 
-                        </p>
-                        <input type="submit"> 
-                        </form>
-`);
+            var template = templateHTML(title, list, `
+          <form action="/create_process" method="post">
+            <p><input type="text" name="title" placeholder="title"></p>
+            <p>
+              <textarea name="description" placeholder="description"></textarea>
+            </p>
+            <p>
+              <input type="submit">
+            </p>
+          </form>
+        `, '');
             response.writeHead(200);
             response.end(template);
-        })
+        });
+    } else if (pathname == '/update') {
+        fs.readdir('./data', function (error, filelist) {
+            fs.readFile(`data/${queryData.id}`, 'utf8', function (err, description) {
+                var title = queryData.id;
+                var list = templateList(filelist);
+                var template = templateHTML(title, list, `
+          <form action="/update_process" method="post">
+        <input type="hidden" name="id" value="${title}">
+<p><input type="text" name="title" placeholder="title" value="${title}"></p>
+            <p>
+              <textarea name="description" placeholder="description">${description}</textarea>
+            </p>
+            <p>
+              <input type="submit">
+            </p>
+          </form>
+        `, `<a href="/create">create</a> <a href="/update?id=${title}">update</a>`);
+                response.writeHead(200);
+                response.end(template);
+            });
+        });
+
+    } else if (pathname === '/create_process') {
+        var body = '';
+        request.on('data', function (data) {
+            body = body + data;
+        });
+        request.on('end', function () {
+            var post = qs.parse(body);
+            var title = post.title;
+            var description = post.description;
+            fs.writeFile(`data/${title}`, description, 'utf-8', function (err) {
+                response.writeHead(302, {
+                    Location: `/?id=${title}`
+                });
+                response.end();
+            })
+        });
+    } else if (pathname === '/update_process') {
+        var body = '';
+        request.on('data', function (data) {
+            body = body + data;
+        });
+        request.on('end', function () {
+            var post = qs.parse(body);
+            var id = post.id;
+            var title = post.title;
+            var description = post.description;
+            fs.rename(`data/${id}`, `data/${title}`, function (error) {
+                fs.writeFile(`data/${title}`, description, 'utf-8', function (err) {
+                    response.writeHead(302, {
+                        Location: `/?id=${title}`});
+                        response.end();
+                })
+            });
+        });
     } else {
         response.writeHead(404);
         response.end('Not found');
